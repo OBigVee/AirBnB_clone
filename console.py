c0dde09fca7e4b7d94ebcafefbbb851ca93184cd
OBigVee <adedejivictor98@gmail.com>
2021-11-14 17:30:40 +0100

    Console 0.0.1

--- console.py
+++ console.py
@@ -2,15 +2,33 @@
 """ the programm is the entry point of the command interpreter """
 
 
+import cmd, sys
 
-import cmd,sys
 
 class HBNBCommand(cmd.Cmd):
-    intro = """ \Documented commands (type help <topic>):\
-                ========================================\
-                EOF  help  quit\ """
+    intro = """\
+	  Documented commands (type help <topic>):\n \
+          ========================================\n \
+          EOF  help  quit \n"""
+
     prompt = "(hbnb)"
 
-    
+    def __init__(self, completekey="tab", stdin=None, stdout=None):
+        super().__init__(completekey, stdin, stdout)
+
+    def do_quit(self, line):
+        """leave the current program"""
+        # Cmd.emptyline() == True
+        return True
+
+    def do_greet(self, line):
+        """display interactive intro"""
+        print(intro)
+
+    def do_EOF(self, line):
+        """quit CLI"""
+        return True
 
 
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
