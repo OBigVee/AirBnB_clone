2ba81675b6d74ec870c62288c2027a738d25b494
OBigVee <adedejivictor98@gmail.com>
2023-08-01 23:57:32 +0100

    :sparkles:

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,14 @@ import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
-          self.id = str(uuid.uuid4())
-          self.created_at = datetime.now()
-          self.updated_at = self.created_at
-          
-          if kwargs:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        if kwargs:
             kwargs.pop("__class__", None)
             for k, v in kwargs.items():
                 setattr(self, k, v)
@@ -23,13 +24,13 @@ class BaseModel:
                 value = kwargs["updated_at"]
                 updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, "updated_at", updated_at)
-            
+
             storage.new(self)
             # else:
             #     self.created_at = datetime.now()
             #     self.updated_at = created_at
             #     storage.new(self)
-        #if len(kwargs) != 0:
+        # if len(kwargs) != 0:
         #     for k,v in kwargs.items():
         #         if k == "__class__":
         #             if k in ["created_at", "updated_at"]:
@@ -47,7 +48,7 @@ class BaseModel:
         """
         self.updated_at = datetime.now()
         storage.save()
-    
+
     def to_dict(self):
         """returns a dictionary containing all keys/values of
         __dict__ of instance
