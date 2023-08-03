6b46eb287a7ca4d0897009c802d4ad8f4ac2365d
OBigVee <adedejivictor98@gmail.com>
2023-08-03 16:21:13 +0100

    :zap: AirBnB console interperter works fine

--- console.py
+++ console.py
@@ -24,9 +24,6 @@ class HBNBCommand(cmd.Cmd):
         "City",
     ]
 
-    valid_classes = ['BaseModel', 'User', 'State', 'City',
-                     'Amenity', 'Place', 'Review']
-
     def do_EOF(self, line):
         """end of file"""
         return True
@@ -51,11 +48,11 @@ class HBNBCommand(cmd.Cmd):
         if len(line) == 0:
             print("** class name missing **")
             return
-        
+
         cls_name = args[0]
         if cls_name not in self.available_classes:
             print("** class doesn't exist **")
-        
+
         else:
             # base = BaseModel(cls_name)
             # base.save()
@@ -77,53 +74,34 @@ class HBNBCommand(cmd.Cmd):
             'Review'
         ]
         """
+        args = line.split()
 
-        argv = line.split()
-        if len(argv) == 0:
+        if len(args) == 0:
             print("** class name missing **")
-        elif len(argv) >= 1 and argv[0] not in self.available_classes:
+            return
+        cls_name = args[0]
+
+        if cls_name not in self.available_classes:
             print("** class doesn't exist **")
-        elif len(argv) == 1:
+
+        elif len(args) < 2:
             print("** instance id missing **")
-        else:
-            # print the string representation of the object
-            storage.reload()
-            objs = storage.all()
-            key = "{}.{}".format(argv[0], argv[1])
-            try:
-                obj = objs[key]
-                print(obj)
-            except KeyError:
-                print("** no instance found **")
+            return
 
-        # args = line.split()
-
-        # if len(args) == 0:
-        #     print("** class name missing **")
-        #     return
-        # cls_name = args[0]
-
-        # if cls_name not in self.available_classes:
-        #     print("** class doesn't exist **")
-
-        # elif len(args) < 2:
-        #     print("** instance id missing **")
-        #     return
-        
-        # attr_id = args[1]
+        attr_id = args[1]
         # storage.reload()
-        # all_objs = storage.all()
-        # obj_keys = f"{cls_name}.{attr_id}"
-        
-        # if obj_keys in all_objs:
-        #     print(all_objs[obj_keys])
-        # else:
-        #     print("** no instance found **")
+        all_objs = storage.all()
+        obj_keys = f"{cls_name}.{attr_id}"
 
-            # try:
-            #     print(all_objs[obj_keys])
-            # except KeyError:
-            #     print("** no instance found **")
+        if obj_keys in all_objs:
+            print(all_objs[obj_keys])
+        else:
+            print("** no instance found **")
+
+        # try:
+        #     print(all_objs[obj_keys])
+        # except KeyError:
+        #     print("** no instance found **")
 
     def do_destroy(self, line):
         """
@@ -145,7 +123,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         attr_id = args[1]
-        #storage.reload()
+        # storage.reload()
         all_objs = storage.all()
         obj_keys = f"{cls_name}.{attr_id}"
 
@@ -156,48 +134,28 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_all(self, line):
-        """do_all: prints all string representation of all 
+        """do_all: prints all string representation of all
         instances based or not on the class name.
         Usage: all <class name> or all
         """
-        # args = line.split()
+        args = line.split()
         # storage.reload()
-        # all_objs = storage.all()
+        all_objs = storage.all()
 
-        # if len(args) == 0:
-        #     print([str(obj) for obj in all_objs.values()])
-        # else:
-        #     cls_name = args[0]
-        #     if cls_name not in self.available_classes:
-        #         print("** class doesn't exist **")
-        #         return
-            
-        #     filtered_objs = [str(obj) for key, obj in all_objs.items()]
-        #     print(filtered_objs)
-        argv = line.split()
-        if len(argv) >= 1 and argv[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(argv) >= 1 and argv[0] in self.valid_classes:
-            # Print a list containing only specified class objects
-            return_list = []
-            storage.reload()
-            objs = storage.all()
-            for key, obj in objs.items():
-                if key == ("" + argv[0] + "." + obj.__dict__["id"]):
-                    return_list.append(obj.__str__())
-            print(return_list)
+        if len(args) == 0:
+            print([str(obj) for obj in all_objs.values()])
         else:
-            # Print a list containing all class objects in storage
-            return_list = []
-            storage.reload()
-            objs = storage.all()
-            for key, obj in objs.items():
-                return_list.append(obj.__str__())
-            print(return_list)
+            cls_name = args[0]
+            if cls_name not in self.available_classes:
+                print("** class doesn't exist **")
+                return
+
+            filtered_objs = [str(obj) for key, obj in all_objs.items()]
+            print(filtered_objs)
 
     def do_update(self, line):
         """Updates an instance based on the class name and `id` by adding
-        or updating attribute (save the changes into the JSON file). 
+        or updating attribute (save the changes into the JSON file).
         Ex: update BaseModel 1234-1234-1234 email "aibnb@mail.com"
         Usage: update <class name> <id> <attribute name> `<attribute value>`
         """
@@ -205,6 +163,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 0:
             print("** class name missing **")
             return
+
         cls_name = args[0]
         if cls_name not in self.available_classes:
             print("** class doesn't exist **")
@@ -213,9 +172,9 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 2:
             print("** instance id missing **")
             return
-        
+
         attr_id = args[1]
-        storage.reload()
+        # storage.reload()
         all_objs = storage.all()
         obj_key = f"{cls_name}.{attr_id}"
 
@@ -228,102 +187,94 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 3:
             print("** attribute name missing **")
             return
-        
+
         attr_name = args[2]
 
-        # storage.reload()
-        # all_objs = storage.all()
-        # obj_key = f"{cls_name}.{attr_name}"
-
-        # try:
-        #     objs = all_objs[obj_key]
-        #     print(objs)
-        # except KeyError:
-        #     print("** attribute name missing **")
-        
         if len(args) < 4:
             print("** value missing **")
         else:
             try:
-                storage.reload()
+                # storage.reload()
                 all_objs = storage.all()
                 obj_key = f"{cls_name}.{attr_id}"
                 obj = all_objs[obj_key]
 
                 try:
                     # get the type of the attribute
-                    Type = type(obj.__dict__[attr_name]) 
+                    Type = type(obj.__dict__[attr_name])
                     # cast the attribute type on attribute value
-                    obj.__dict__[attr_name] = Type(args[3].strip("\""))
+                    obj.__dict__[attr_name] = Type(args[3].strip('"'))
                 except KeyError:
-                    obj.__dict__[attr_name] = args[3].strip("\"")
+                    obj.__dict__[attr_name] = args[3].strip('"')
                 obj.save()
 
             except KeyError:
                 print("** no instance found **")
 
-    def parseline(self, line):
-        """
-        Parse every line before returning it back to the console.
-        Looking out for match such as the following for extra
-        processing:
-        + <class name>.all()
-        + <class name>.count()
-        + <class name>.show(<id>)
-        + <class name>.destroy(<id>)
-        + <class name>.update(<id>, <attribute name>, <attribute value>)
-        + <class name>.update(<id>, <dictionary representation>)
-        """
-        lone_commands = ['all', 'quit', 'EOF', 'help']
-        argv = line.split()
-        matched = re.search(r"\w+\.\w+\(", line)
+    # def parseline(self, line):
+    #     """
+    #     Parse every line before returning it back to the console.
+    #     Looking out for match such as the following for extra
+    #     processing:
+    #     + <class name>.all()
+    #     + <class name>.count()
+    #     + <class name>.show(<id>)
+    #     + <class name>.destroy(<id>)
+    #     + <class name>.update(<id>, <attribute name>, <attribute value>)
+    #     + <class name>.update(<id>, <dictionary representation>)
+    #     """
+    #     lone_commands = ["all", "quit", "EOF", "help"]
+    #     argv = line.split()
+    #     matched = re.search(r"\w+\.\w+\(", line)
 
-        if len(argv) == 0:  # Handles empty line
-            return line, line, line
+    #     if len(argv) == 0:  # Handles empty line
+    #         return line, line, line
 
-        if (len(argv) != 1 or argv[0] in lone_commands) and not matched:
-            # Do for commands that required arguments or
-            # commands that works alone.
-            return argv[0], " ".join(argv[1:]), line
+    #     if (len(argv) != 1 or argv[0] in lone_commands) and not matched:
+    #         # Do for commands that required arguments or
+    #         # commands that works alone.
+    #         return argv[0], " ".join(argv[1:]), line
 
-        # Additional parsing for aliases listed in string doc.
-        # example: class.command(args)
-        if matched:
-            _class, argv = line.split(".", 1)  # ['class', 'command(args)']
-            cmd, argv = argv.split("(", 1)  # ['command', 'args)']
-            argv = argv.replace(")", '')  # 'args)' -> 'args'
+    #     # Additional parsing for aliases listed in string doc.
+    #     # example: class.command(args)
+    #     if matched:
+    #         _class, argv = line.split(".", 1)  # ['class', 'command(args)']
+    #         cmd, argv = argv.split("(", 1)  # ['command', 'args)']
+    #         argv = argv.replace(")", "")  # 'args)' -> 'args'
 
-            matched = re.search(r"{.+}", argv)  # checking for dict definition
-            if matched:  # if dictionary exists in arguements
-                objs = storage.all()
-                import json
-                obj_id, argv_dict = argv.split(", ", 1)  # ['<id>', '<dict>']
-                argv_dict = argv_dict.replace("'", "\"")
-                obj_id = obj_id.replace("\"", "")
-                argv_dict = json.loads(argv_dict)
-                key = "{}.{}".format(_class, obj_id)
-                try:
-                    obj = objs[key]
-                    obj.__dict__.update(argv_dict)
-                    obj.save()
-                except KeyError:
-                    print("** no instance found **")
+    #         matched = re.search(r"{.+}", argv)  # checking for dict definition
+    #         if matched:  # if dictionary exists in arguements
+    #             objs = storage.all()
+    #             import json
 
-                line = ""
-                return line, line, line
+    #             obj_id, argv_dict = argv.split(", ", 1)  # ['<id>', '<dict>']
+    #             argv_dict = argv_dict.replace("'", '"')
+    #             obj_id = obj_id.replace('"', "")
+    #             argv_dict = json.loads(argv_dict)
+    #             key = "{}.{}".format(_class, obj_id)
+    #             try:
+    #                 obj = objs[key]
+    #                 obj.__dict__.update(argv_dict)
+    #                 obj.save()
+    #             except KeyError:
+    #                 print("** no instance found **")
 
-            if cmd == "count":  # handles <class>.count()
-                objs = storage.all()
-                count = 0
-                for key, obj in objs.items():
-                    if obj.__class__.__name__ == _class:
-                        count = count + 1
-                print(count)
-                line = ''
-                return line, line, line
-            return cmd, _class + " " + " ".join(argv.split(", ")), line
+    #             line = ""
+    #             return line, line, line
 
-        return argv[0], " ".join(argv[1:]), line
+    #         if cmd == "count":  # handles <class>.count()
+    #             objs = storage.all()
+    #             count = 0
+    #             for key, obj in objs.items():
+    #                 if obj.__class__.__name__ == _class:
+    #                     count = count + 1
+    #             print(count)
+    #             line = ""
+    #             return line, line, line
+    #         return cmd, _class + " " + " ".join(argv.split(", ")), line
+
+    #     return argv[0], " ".join(argv[1:]), line
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
